4bcb123e1f8589f1f74361778eee1cab5268119d
Wejden Slimen <121268846+Wejdenslimen@users.noreply.github.com>
2024-09-21 17:40:01 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!--start contact us -->
+<div class="container mt-4" id="contact">
+    <!--start contact us container-->
+    <h1 class="text-center mb4 tit">Contact Us</h1><br>
+    <!--contact us heading-->
+    <div class="row">
+        <!--start contact us row-->
+        <div class="col-md-8">
+            <!--start contact us 1st column-->
+            <form action="" method="post">
+                <input type="text" class="form-control" name="name" placeholder="Name"><br>
+                <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+                <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+                <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+                <input class="btn btn-primary btn-pink btn-lg" type="submit" value="Send" name="submit"><br><br>
+            </form>
+        </div>
+        <!--end contact us 1st column-->
+        <div class="col-md-4 stripe text-white text-center">
+            <!--start contact us 2nd column-->
+            <h4>4IOT</h4>
+            <p>4IOT,
+                Near Police Camp II , Bokaro,
+                Jharkhand - 834005<br/>
+                Phone: +00000000<br/>
+                www.4IOT.com
+            </p>
+        </div>
+        <!--end contact us 2nd column-->
+    </div> <!--end contact us row-->
+</div><!--end contact us container-->
+
+<!--end contact us -->
